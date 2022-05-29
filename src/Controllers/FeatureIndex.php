9a12f6224a57fb24e4dee06e8385ba1663c4d2e2
Manix <admin@manix.info>
2022-05-29 15:22:20 +0300

    fix feature index feature access control list

--- src/Controllers/FeatureIndex.php
+++ src/Controllers/FeatureIndex.php
@@ -3,6 +3,7 @@
 namespace Manix\Brat\Utility\Admin\Controllers;
 
 use Manix\Brat\Utility\Admin\Views\HomeView;
+use Manix\Brat\Utility\Users\Models\Auth;
 
 trait FeatureIndex {
 
@@ -14,6 +15,15 @@ trait FeatureIndex {
 
   public function get() {
     $this->page = $this->featureLitView();
+
+    $user = Auth::user();
+    $features = [];
+    
+    foreach ($this->features() as $feature) {
+    	if ($feature->accessControl($user)) {
+    	    $features[] = $feature;
+    	}
+    }
 
     return [
         'features' => $this->features(),
